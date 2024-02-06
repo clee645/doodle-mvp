02098475008096ed633f826b5722671291b835d0
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-02-05 23:50:58 -0500

    added basic starting page and button

--- doodle/src/App.js
+++ doodle/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './doodle-title.png';
 import './App.css';
 
 function App() {
+  const handleStartCapture = () => {
+    // Add your logic for starting the capture
+    alert('Capture started!');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <h1 className="App-title">Doodle</h1> */}
+        <button onClick={handleStartCapture} className="start-capture-button">
+          Start Capture
+        </button>
       </header>
     </div>
   );
