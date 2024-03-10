fd7dfd5ee43f7044cb1e15c4b09e30ddc8accf39
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-03-09 19:54:40 -0500

    frontend changes / got rid of logo

--- doodle/public/sidepanel.js
+++ doodle/public/sidepanel.js
@@ -9,34 +9,34 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   }
 });
 
-/*global chrome*/
+// /*global chrome*/
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  if (message.action === 'displayWebsiteInfo') {
-    const websiteLogo = message.websiteLogo;
-    const websiteUrl = message.websiteUrl;
+// chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+//   if (message.action === 'displayWebsiteInfo') {
+//     const websiteLogo = message.websiteLogo;
+//     const websiteUrl = message.websiteUrl;
 
-    // Extract domain name without "www." prefix
-    const domain = extractDomain(websiteUrl);
+//     // Extract domain name without "www." prefix
+//     const domain = extractDomain(websiteUrl);
 
-    // Display the website logo
-    document.getElementById('logoContainer').innerHTML = `<img src="${websiteLogo}" alt="Website Logo" style="max-width: 100%; height: auto; display: block; margin-top: 10px;" />`;
+//     // Display the website logo
+//     document.getElementById('logoContainer').innerHTML = `<img src="${websiteLogo}" alt="Website Logo" style="max-width: 100%; height: auto; display: block; margin-top: 10px;" />`;
 
-    // Display the domain name
-    document.getElementById('urlContainer').textContent = domain;
-  }
-});
+//     // Display the domain name
+//     document.getElementById('urlContainer').textContent = domain;
+//   }
+// });
 
-// Function to extract the domain name from the URL
-const extractDomain = (url) => {
-  try {
-    const domain = new URL(url).hostname;
-    return domain.replace("www.", "");
-  } catch (error) {
-    console.error('Error extracting domain:', error);
-    return "";
-  }
-};
+// // Function to extract the domain name from the URL
+// const extractDomain = (url) => {
+//   try {
+//     const domain = new URL(url).hostname;
+//     return domain.replace("www.", "");
+//   } catch (error) {
+//     console.error('Error extracting domain:', error);
+//     return "";
+//   }
+// };
 
 document.getElementById('stopCapture').addEventListener('click', function() {
   document.getElementById('message').textContent = 'Capture stopped';
